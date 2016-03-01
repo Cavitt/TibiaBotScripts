13aae1be246f8e8594f07725ca703d6009f9d07f
Cavitt <cavitt.glover@gmail.com>
2016-03-01 00:02:10 -0600

    Update deploy scripts

--- deploy-beta.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e # exit with nonzero exit code if anything fails
-
-# Upload artifacts to FTP server
-find build -type f -exec curl -u ${FTP_USER}@${FTP_PASS} --ftp-create-dirs -T {} ftp://${FTP_HOST}/beta{} \;
---
+++ tools/deploy.js
@@ -0,0 +1,41 @@
+import task from './lib/task';
+import fs from 'vinyl-fs';
+import ftp from 'vinyl-ftp';
+
+/**
+ * Deploy files to FTP server
+ */
+export default task('deploy', () => {
+
+  if (process.env.TRAVIS_TEST_RESULT !== 1) {
+    console.log('Skipping deploy. Build unsuccessful.');
+    return;
+  }
+
+  if (process.env.TRAVIS_SECURE_ENV_VARS !== 'true') {
+    console.log('Skipping deploy. Secure environment variables missing.');
+    return;
+  }
+
+  if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
+    console.log('Skipping deploy. Disabled for pull requests.');
+    return;
+  }
+
+  if (!process.env.TRAVIS_TAG && process.env.TRAVIS_BRANCH !== 'master') {
+    console.log('Skipping deploy. Only master branch deploys.');
+    return;
+  }
+
+  const path = process.env.TRAVIS_TAG ? '/release' : '/beta';
+  const connection = ftp.create({
+    host: process.env.FTP_HOST,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASS,
+    parallel: 10,
+    log: console.log
+  });
+
+  return fs.src(['./build/**'], {buffer: false} )
+    .pipe(connection.dest(path));
+});
